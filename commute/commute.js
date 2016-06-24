40923286ac98d54be83d1e911a476d600ff4c1ef
napalm255 <napalm255@gmail.com>
2016-06-24 18:50:27 -0400

    misc updates

--- commute/commute.js
+++ commute/commute.js
@@ -1,4 +1,5 @@
 $(function () {
+    document.write('testing')
     var url = 'https://api.naponline.net/commute?address_hash=07d72756aed4a5eeb114ba34b9926777&format=gecko'
     $.getJSON(url, function (data) {
         $('#container').innerHTML(data)
