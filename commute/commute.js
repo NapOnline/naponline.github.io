8b9120e174e7ffa41bbe5390cd5c16bfa6b41a79
napalm255 <napalm255@gmail.com>
2017-12-18 22:35:39 -0500

    changed url

--- commute/commute.js
+++ commute/commute.js
@@ -1,5 +1,5 @@
 $(function() {
-  var url = 'https://api.naponline.net/commute?address_hash=07d72756aed4a5eeb114ba34b9926777&format=hc'
+  var url = 'https://api.naponline.net/commute'
   $.getJSON(url, function(data) {
     var latestTime = new Date(data.series[0].data[data.series[0].data.length-1][0]);
     var latestCommute = data.series[0].data[data.series[0].data.length-1][1]
@@ -72,7 +72,7 @@ $(function() {
 });
 
 $(function() {
-  var url = 'https://api.naponline.net/commute?address_hash=35d8a61127c303f274b13ba628be33e7&format=hc'
+  var url = 'https://api.naponline.net/commute'
   $.getJSON(url, function(data) {
     var latestTime = new Date(data.series[0].data[data.series[0].data.length-1][0]);
     var latestCommute = data.series[0].data[data.series[0].data.length-1][1]
