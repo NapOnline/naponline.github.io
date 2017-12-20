6b658b83631727c0bd3ea556811e56c2c4fd9d46
napalm255 <napalm255@gmail.com>
2017-12-19 22:33:10 -0500

    added json

--- ip/ip.js
+++ ip/ip.js
@@ -1,6 +1,13 @@
 $(function() {
   var url = 'https://api.naponline.net/ip'
   $.get(url, function(data) {
-    $('#ip_address').html(data)
+    $('#ip_address_text').html(data)
+  }, "text");
+});
+
+$(function() {
+  var url = 'https://api.naponline.net/ip/details'
+  $.get(url, function(data) {
+    $('#ip_address_json').html(data)
   }, "text");
 });
