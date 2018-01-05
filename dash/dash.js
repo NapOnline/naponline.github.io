2b9d5e5316a152ef65807b0b7d0224567a1b0f36
napalm255 <napalm255@gmail.com>
2018-01-04 21:53:39 -0500

    added dash

--- dash/dash.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-  var url = 'http://www.chart.state.md.us/video/video.php?feed=7a00a1dc01250075004d823633235daa'
-  $.get(url, function(data) {
-    $('#camfeed0').html(data)
-  }, "html");
-});
