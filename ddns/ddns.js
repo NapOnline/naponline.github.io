d599c885413c63edd78f9b28f7466294396f4728
napalm255 <napalm255@gmail.com>
2018-01-13 21:15:08 -0500

    updated

--- ddns/ddns.js
+++ ddns/ddns.js
@@ -1,6 +1,38 @@
-$(function() {
+function update() {
   var url = 'https://api.naponline.net/ddns'
-  // $.get(url, function(data) {
-  //   $('#ddns').html(data)
-  // }, "html");
+  alert($('#name').val());
+  $.ajax({
+    url: url,
+    type: 'post',
+    data: {
+      name: $('#name').val()
+    },
+    headers: {
+      "x-api-key": $('#key').val()
+    },
+    dataType: 'json',
+    success: function(data) {
+      $('#status').html(data.stringify());
+    }
+  });
+}
+
+$(function(){
+  $('#key').keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+      update()
+    }
+    event.stopPropagation();
+  });
+  $('#name').keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+      update()
+    }
+    event.stopPropagation();
+  });
+  $("button").click(function(){
+    update()
+  });
 });
