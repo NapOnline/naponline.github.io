a8e5d135094483db5ecff55057990360adeddad8
napalm255 <napalm255@gmail.com>
2019-06-09 21:22:38 -0400

    testing

--- recipes/index.js
+++ recipes/index.js
@@ -1,7 +1,7 @@
 // Get table of contents
 $(function() {
   var url = 'https://api.naponline.net/recipes'
-  $.get(url, function(data) {
-    $('#toc').html(data);
+  $.getJSON(url, function(data) {
+    $('#toc').html(data.index);
   }, "html");
 });
--- recipes/recipe.js
+++ recipes/recipe.js
@@ -3,7 +3,9 @@ $(function() {
   var recipe = $.uriGet('r');
   var url = 'https://api.naponline.net/recipes/' + recipe;
   $.getJSON(url, function(data) {
-    $('#recipe_header').html(data.recipe.name);
-    $('#recipe').html(data.recipe.created);
+    $('#r_name').html(data.recipe.name);
+    $('#r_description').html(data.recipe.description);
+    $('#r_created').html(data.recipe.created);
+    $('#r_modified').html(data.recipe.modified);
   }, "html");
 });
