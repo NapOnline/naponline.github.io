5fd11f2b5ab7f756c3a4561d5f0ca95084da9130
napalm255 <napalm255@gmail.com>
2019-06-09 21:16:37 -0400

    testing

--- recipes/recipe.js
+++ recipes/recipe.js
@@ -2,7 +2,7 @@
 $(function() {
   var recipe = $.uriGet('r');
   var url = 'https://api.naponline.net/recipes/' + recipe;
-  $.get(url, function(data) {
+  $.getJSON(url, function(data) {
     $('#recipe_header').html(data.recipe.name);
     $('#recipe').html(data);
   }, "html");
