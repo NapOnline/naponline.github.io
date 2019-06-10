ed1dc472af0594d3636f67dfc4ce213688174046
napalm255 <napalm255@gmail.com>
2019-06-09 21:27:22 -0400

    testing

--- recipes/recipe.js
+++ recipes/recipe.js
@@ -1,11 +1,15 @@
 // Get recipe
 $(function() {
   var recipe = $.uriGet('r');
-  var url = 'https://api.naponline.net/recipes/' + recipe;
-  $.getJSON(url, function(data) {
-    $('#r_name').html(data.recipe.name);
-    $('#r_description').html(data.recipe.description);
-    $('#r_created').html(data.recipe.created);
-    $('#r_modified').html(data.recipe.modified);
-  }, "html");
+  if (recipe != '') {
+    var url = 'https://api.naponline.net/recipes/' + recipe;
+    $.getJSON(url, function(data) {
+      $('#r_name').html(data.recipe.name);
+      $('#r_description').html(data.recipe.description);
+      $('#r_created').html(data.recipe.created);
+      $('#r_modified').html(data.recipe.modified);
+    }, "html");
+  } else {
+      $('#r_name').html('No recipe id provided.');
+  }
 });
