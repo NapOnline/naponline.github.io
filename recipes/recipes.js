9b447d38e10c308826e2b0ffd3688a64d6bf3c88
napalm255 <napalm255@gmail.com>
2019-06-09 20:55:50 -0400

    playing

--- recipes/recipes.js
+++ recipes/recipes.js
@@ -1,7 +1,26 @@
+(function ($) {
+    $.extend({
+        uriGet: function () {
+          var url_string = location.href;
+          var url = new URL(url_string);
+          var val = url.searchParams.get(arguments[0]);
+          return val;
+        }
+    });
+})(jQuery);
+
+$(function() {
+  var recipe = $.uriGet('r');
+  var url = 'https://api.naponline.net/recipes/' + recipe;
+  $.get(url, function(data) {
+    $('#recipe').html(data);
+  }, "html");
+});
+
 // Get table of contents
 $(function() {
   var url = 'https://api.naponline.net/recipes'
   $.get(url, function(data) {
-    $('#toc').html(data)
+    $('#toc').html(data);
   }, "html");
 });
