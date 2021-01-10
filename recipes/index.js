fceac1d2afdd69261ce9a530c987a015682a1860
napalm255 <napalm255@gmail.com>
2021-01-10 04:00:23 +0000

    recipes

--- recipes/index.js
+++
@@ -1,7 +0,0 @@
-// Get table of contents
-$(function() {
-  var url = 'https://api.naponline.net/recipes/';
-  $.getJSON(url, function(data) {
-    $('#i_toc').html(data.index.recipes);
-  }, "html");
-});
